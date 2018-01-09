5fdafe7579c27c9cef7c1ca4496aadbed036378e
dirtyDe <m18406592251@163.com>
2018-01-09 10:30:43 +0800

    simple changes

---
+++ RollBall/Assets/imitation/scripts/playercontroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+//Include the namespace required to use Unity UI
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class playercontroller : MonoBehaviour
+{
+	//create public variables for player speed, and for the Text UI game object
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
